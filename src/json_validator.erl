f8f3138f958496e6add7c3deeb55741ff26071d2
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-03-29 17:34:16 +0200

    Add LICENSE.txt and source file headers (MIT license)

--- src/json_validator.erl
+++ src/json_validator.erl
@@ -1,3 +1,12 @@
+%%
+%% Copyright (c) 2023, Byteplug LLC.
+%%
+%% This source file is part of the JSON Validator project from Erlangsters and
+%% is released under the MIT license. Please refer to the LICENSE.txt file that
+%% can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, March 2023
+%%
 -module(json_validator).
 
 -export([json_validator/0]).
