0d136e7c8ace0616e583288cbb84df8ae94eee73
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-07-10 20:03:11 +0200

    Add placeholders for the JSON validators

---
+++ test/json_bool_validator_test.erl
@@ -0,0 +1,14 @@
+%%
+%% Copyright (c) 2023, Byteplug LLC.
+%%
+%% This source file is part of the JSON Validator project from Erlangsters and
+%% is released under the MIT license. Please refer to the LICENSE.txt file that
+%% can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, July 2023
+%%
+-module(json_bool_validator_test).
+-include_lib("eunit/include/eunit.hrl").
+
+json_bool_validator_test() ->
+    ok.
