587b1468f1b20b762124ff1ed924735629ef5204
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-07-24 20:25:58 +0200

    Implement JSON object validator

--- src/json_object_validator.erl
+++ src/json_object_validator.erl
@@ -8,19 +8,79 @@
 %% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, July 2023
 %%
 -module(json_object_validator).
--behaviour(term_validator).
+-behavior(term_validator).
 
--export([mandatory_options/0]).
--export([options/0]).
+-export([options/1]).
+
 -export([pre_validate/3]).
 -export([validate/3]).
 -export([post_validate/2]).
 
-mandatory_options() -> [].
-options() -> [].
+options(optional) -> [dynamic, use_maps];
+options(mandatory) -> [fields].
 
-pre_validate(Term, _Options, _Validators) ->
-    {valid, Term}.
+%%
+%% JSON object validator.
+%%
+%% This module implements the JSON object validator.
+%%
+%% XXX: Update the implementation to re-use the implementation of the built-in
+%%      maps validator of ETV.
+%%
+pre_validate(Term, Options, Validators) when is_map(Term) ->
+    % We first compute the missing fields by iterating over the declared
+    % mandatory fields and check if they are all present in the Erlang map.
+    % Then we compute the invalid and extra fields by iterating over the fields
+    % of the Erlang map.
+    Fields = proplists:get_value(fields, Options),
+    Dynamic = proplists:get_value(dynamic, Options, false),
+
+    Keys0 = lists:foldr(
+        fun
+            ({Name, _Format, mandatory}, Accumulator) ->
+                case maps:is_key(list_to_binary(Name), Term) of
+                    true ->
+                        Accumulator;
+                    false ->
+                        [{Name, missing}|Accumulator]
+                end;
+            ({Name, _Format, optional}, Accumulator) ->
+                Accumulator
+        end,
+        [],
+        Fields
+    ),
+    Keys1 = maps:fold(
+        fun(Key, Value, Accumulator) ->
+            case proplists:lookup(binary_to_list(Key), Fields) of
+                none ->
+                    case Dynamic of
+                        true ->
+                            Accumulator;
+                        false ->
+                            [{binary_to_list(Key), unexpected}|Accumulator]
+                    end;
+                {Name, Format, _} ->
+                    case term_validator:validate(Value, Format, Validators) of
+                        valid ->
+                            Accumulator;
+                        {invalid, Reason} ->
+                            [{Name, invalid, Reason}|Accumulator]
+                    end
+            end
+        end,
+        Keys0,
+        Term
+    ),
+
+    case Keys1 of
+        [] ->
+            {valid, Term, []};
+        _ ->
+            {invalid, {keys, Keys1}}
+    end;
+pre_validate(Term, Options, _Validators) ->
+    {invalid, not_object}.
 
 validate(Term, _Option, _Validators) ->
     {valid, Term}.
--- test/json_object_validator_test.erl
+++ test/json_object_validator_test.erl
@@ -11,4 +11,81 @@
 -include_lib("eunit/include/eunit.hrl").
 
 json_object_validator_test() ->
+    % Test against all valid JSON-encodable Erlang terms (the scope of this
+    % library is to validate valid JSON-encodable Erlang terms which is
+    % ensured by the library encoding the raw JSON text).
+    Format = {object, [{fields, []}, use_maps]},
+    {invalid, not_object} = json_validator:validate(null, Format),
+    {invalid, not_object} = json_validator:validate(false, Format),
+    {invalid, not_object} = json_validator:validate(true, Format),
+    {invalid, not_object} = json_validator:validate(42, Format),
+    {invalid, not_object} = json_validator:validate(42.5, Format),
+    {invalid, not_object} = json_validator:validate(<<"Hello world!">>, Format),
+    {invalid, not_object} = json_validator:validate([], Format),
+    valid = json_validator:validate(#{}, Format),
+
+    ok.
+
+json_object_validator_fields_test() ->
+    Format = {object, [{fields, [
+        {"foo", bool, mandatory},
+        {"bar", number, optional}
+    ]}, use_maps]},
+    Validators = #{
+        bool => json_bool_validator,
+        number => json_number_validator,
+        string => json_string_validator,
+        object => json_object_validator
+    },
+
+    % Test mandatory fields.
+    {invalid, {keys, [{"foo", missing}]}} = term_validator:validate(#{
+        <<"bar">> => 42
+    }, Format, Validators),
+
+    % Test optional fields.
+    valid = term_validator:validate(#{
+        <<"foo">> => false,
+        <<"bar">> => 42
+    }, Format, Validators),
+
+    % Test invalid fields.
+    {invalid, {keys, [{"bar", invalid, not_number}]}} = term_validator:validate(#{
+        <<"foo">> => false,
+        <<"bar">> => true
+    }, Format, Validators),
+
+    % Test extra fields.
+    {invalid, {keys, [{"quz", unexpected}]}} = term_validator:validate(#{
+        <<"foo">> => false,
+        <<"bar">> => 42,
+        <<"quz">> => <<"Hello world!">>
+    }, Format, Validators),
+
+    % Test extra fields.
+    {invalid, {keys, [{"quz", unexpected}]}} = term_validator:validate(#{
+        <<"foo">> => false,
+        <<"bar">> => 42,
+        <<"quz">> => <<"Hello world!">>
+    }, Format, Validators),
+
+    FormatWithDynamic = {object, [{fields, [
+        {"foo", bool, mandatory},
+        {"bar", number, optional}
+    ]}, dynamic, use_maps]},
+    valid = term_validator:validate(#{
+        <<"foo">> => false,
+        <<"bar">> => 42,
+        <<"quz">> => <<"Hello world!">>
+    }, FormatWithDynamic, Validators),
+
+    % Test lazy validation.
+    {invalid, {keys, Keys}} = term_validator:validate(#{
+        <<"bar">> => false,
+        <<"quz">> => <<"Hello world!">>
+    }, Format, Validators),
+    true = lists:member({"foo", missing}, Keys),
+    true = lists:member({"bar", invalid, not_number}, Keys),
+    true = lists:member({"quz", unexpected}, Keys),
+
     ok.
