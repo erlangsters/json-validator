3cfd47cb174dc012df1d7216d56eecbaa5070113
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-07-25 19:31:48 +0200

    Implement JSX and Jiffy integration unit test

--- test/json_encode_jsx_test.erl
+++ test/json_encode_jsx_test.erl
@@ -10,5 +10,64 @@
 -module(json_encode_jsx_test).
 -include_lib("eunit/include/eunit.hrl").
 
+%%
+%% XXX: More effort could be put into this important unit test that aims at
+%%      checking if the entire library works as expected with real-life or
+%%      complex input.
+%%
+
+valid_json() ->
+    <<"
+{
+    \"yolo\": {
+        \"foo\": true,
+        \"bar\": 42,
+        \"quz\": \"Hello world!\"
+    },
+    \"oloy\": [null, false, 0]
+}
+    ">>.
+
+invalid_json() ->
+    <<"
+{
+    \"yolo\": {
+        \"foo\": \"Hello world!\",
+        \"bar\": 42,
+        \"quz\": true
+    },
+    \"oloy\": [\"null\", \"false\", 0]
+}
+    ">>.
+
 json_encode_jsx_test() ->
+    Format = {object, [{fields, [
+        {"yolo", {object, [{fields, [
+            {"foo", bool, mandatory},
+            {"bar", number, mandatory},
+            {"quz", string, mandatory}
+        ]}]}, mandatory},
+        {"oloy", {array, [{item, {any_of, [
+            null,
+            bool,
+            number
+        ]}}]},
+        mandatory}
+    ]}]},
+
+    Json = valid_json(),
+    Document = jsx:decode(Json, [{return_maps, true}]),
+    valid = json_validator:lazy_validate(Document, Format),
+
+    InvalidJson = invalid_json(),
+    InvalidDocument = jsx:decode(InvalidJson, [{return_maps, true}]),
+    {invalid, Errors} = json_validator:lazy_validate(InvalidDocument, Format),
+    true = lists:member({[{key, "yolo"}, {key, "foo"}], not_bool}, Errors),
+    true = lists:member({[{key, "yolo"}, {key, "quz"}], not_string}, Errors),
+
+    NotAnyOf = {not_any_of, [null, bool, number]},
+    true = lists:member({[{key, "oloy"}, {index, 1}], NotAnyOf}, Errors),
+    true = lists:member({[{key, "oloy"}, {index, 2}], NotAnyOf}, Errors),
+    false = lists:member({[{key, "oloy"}, {index, 3}], NotAnyOf}, Errors),
+
     ok.
