d503c27734ddf03051fab43256d7048b1b0f52bc
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2023-07-25 12:16:59 +0200

    Implement JSON string validator

--- src/json_string_validator.erl
+++ src/json_string_validator.erl
@@ -10,20 +10,38 @@
 -module(json_string_validator).
 -behaviour(term_validator).
 
--export([mandatory_options/0]).
--export([options/0]).
+-export([options/1]).
+
 -export([pre_validate/3]).
 -export([validate/3]).
 -export([post_validate/2]).
 
-mandatory_options() -> [].
-options() -> [].
+%%
+%% JSON string validator.
+%%
+%% This module implements the JSON string validator. We want the same
+%% interface as the built-in string validator of ETV, so we re-use its
+%% implementation for the most part. The only difference is that we expect the
+%% string value in binary format. Therefore we convert is first then pass it
+%% to the implementation of the built-in string validator.
+%%
 
-pre_validate(Term, _Options, _Validators) ->
-    {valid, Term}.
+options(optional) ->
+    string_validator:options(optional);
+options(mandatory) ->
+    string_validator:options(mandatory).
 
-validate(Term, _Option, _Validators) ->
-    {valid, Term}.
+pre_validate(Term, Options, Validators) when is_binary(Term) ->
+    string_validator:pre_validate(
+        unicode:characters_to_list(Term),
+        Options,
+        Validators
+    );
+pre_validate(_Term, _Options, _Validators) ->
+    {invalid, not_string}.
 
-post_validate(_Term, _Validators) ->
-    valid.
+validate(Term, Option, Validators) ->
+    string_validator:validate(Term, Option, Validators).
+
+post_validate(Term, Validators) ->
+    string_validator:post_validate(Term, Validators).
--- test/json_string_validator_test.erl
+++ test/json_string_validator_test.erl
@@ -11,4 +11,31 @@
 -include_lib("eunit/include/eunit.hrl").
 
 json_string_validator_test() ->
+    % Test against all valid JSON-encodable Erlang terms (the scope of this
+    % library is to validate valid JSON-encodable Erlang terms which is
+    % ensured by the library encoding the raw JSON text).
+    Format = string,
+    {invalid, not_string} = json_validator:validate(null, Format),
+    {invalid, not_string} = json_validator:validate(false, Format),
+    {invalid, not_string} = json_validator:validate(true, Format),
+    {invalid, not_string} = json_validator:validate(42, Format),
+    {invalid, not_string} = json_validator:validate(42.5, Format),
+    valid = json_validator:validate(<<"Hello world!">>, Format),
+    {invalid, not_string} = json_validator:validate([], Format),
+    {invalid, not_string} = json_validator:validate(#{}, Format),
+
+    ok.
+
+json_string_validator_options_test() ->
+    % As it shares the implementation of the 'string' validator of ETV, we do
+    % not need to test all its options thoroughly.
+    Format = {string, [{length, {4, 6}}]},
+    {invalid, {length, {must_be_greater_or_equal_to, 4}}} =
+        json_validator:validate(<<"abc">>, Format),
+    valid = json_validator:validate(<<"abcd">>, Format),
+
+    valid = json_validator:validate(<<"abcdef">>, Format),
+    {invalid, {length, {must_be_less_or_equal_to, 6}}} =
+        json_validator:validate(<<"abcdefg">>, Format),
+
     ok.
